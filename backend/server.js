124c01ab0105c6df96980fdb09d2538d74f4ea33
pavithran <pavithranr65@gmail.com>
2022-06-23 20:30:36 +0530

    Initializing project

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const chats = require("./data/data");
+require("dotenv").config();
+const PORTNO = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const response = chats.find((c) => c._id === req.params.id);
+  res.send(response);
+});
+
+app.listen(PORTNO, console.log(`Server is running on PORT ${PORTNO}`));
